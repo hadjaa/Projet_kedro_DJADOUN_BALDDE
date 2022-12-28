8e0fc995b565b18b0b232ce3a142fbf1c4bc1c2c
hadja <hadjamaimounabalde1@gmail.com>
2022-12-28 21:26:41 +0100

    Model SVM

--- src/projetfinal/pipelines/data_engineering/nodes.py
+++ src/projetfinal/pipelines/data_engineering/nodes.py
@@ -5,12 +5,18 @@ generated using Kedro 0.18.3
 import os
 import pandas as pd
 import numpy as np
-from PIL import Image
+from pyexpat import model
 import cv2
 from sklearn.model_selection import train_test_split
 from typing import Any, Tuple, Dict
 import sys
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+import mlflow
+import logging
 
+np.set_printoptions(threshold=np.inf)
+np.set_string_function(lambda x : repr(x), repr=False)
 ####Fonction pour compter le nombre total d'image###
 
 def nombreTotalImage (base_path : str) -> int:
@@ -52,10 +58,16 @@ def create_dataframe(base_path : str) -> pd.DataFrame:
                     k += 1  
     return data 
 
+#def update_culumn_dataframe(data: pd.DataFrame) -> pd.DataFrame:
+    
+    
+
+###################Split data#######################  
+
 def split_train_test_data(data: pd.DataFrame, test_size : float) -> Tuple:
     x = []
     y = []
-    np.set_printoptions(threshold=sys.maxsize)
+
     for i, row in data.iterrows():
         x.append(row['feature'])
         y.append(row['target'])
@@ -65,6 +77,27 @@ def split_train_test_data(data: pd.DataFrame, test_size : float) -> Tuple:
         #x = np.append(y)
     pd.Series(y).value_counts()
     x_updated = x.reshape(len(x), -1)
+    #split data
     x_train, x_test, y_train, y_test = train_test_split(x_updated, y, test_size=test_size, shuffle=True)
-
     return  pd.DataFrame(data=x_train, columns=['values']),  pd.DataFrame(data=x_test, columns=['values']),  pd.DataFrame(data=y_train, columns=['values']),  pd.DataFrame(data=y_test, columns=['values'])
+
+###################Train model SVM#######################  
+
+def support_vector_machine(x_train: pd.DataFrame, x_test: pd.DataFrame, y_train: pd.DataFrame):
+    #Model initialization
+    x_train['values'] = x_train['values'].apply(np.array)
+    x_test['values'] = x_test['values'].apply(np.array)
+    y_train['values'] = y_train['values'].apply(np.array)
+    print("== The type is", x_train.dtypes, x_test.dtypes, y_train.dtypes)
+    model =  SVC(C=0.1)
+    #Save the model with mlflow
+    mlflow.sklearn.log_model(model, "model")
+    ##train model
+    model.fit(x_train, y_train)
+    #Prediction des target avec la partie du test
+    y_pred = model.predict(x_test)
+   #Creation of the dataframe prediction
+    pred = pd.DataFrame(y_pred, columns=['values'])
+    
+
+    return pred
--- src/projetfinal/pipelines/data_engineering/pipeline.py
+++ src/projetfinal/pipelines/data_engineering/pipeline.py
@@ -4,7 +4,7 @@ generated using Kedro 0.18.3
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-from projetfinal.pipelines.data_engineering.nodes import nombreTotalImage, create_dataframe, split_train_test_data
+from projetfinal.pipelines.data_engineering.nodes import nombreTotalImage, create_dataframe, split_train_test_data, support_vector_machine
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -26,5 +26,11 @@ def create_pipeline(**kwargs) -> Pipeline:
                 inputs=["created_dataset", "params:test_size"],
                 outputs=["x_train", "x_test", "y_train", "y_test"],
                 name="split_train_test_data"
+            ),
+            node(
+                func=support_vector_machine,
+                inputs=["x_train", "x_test", "y_train"],
+                outputs=["pred"],
+                name="support_vector_machine"
             ),
     ])
